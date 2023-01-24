95e85b55a33e3b5cd8f9b80a8e232ccde261cede
aibakeneko <aibakeneko@gmail.com>
2023-01-24 12:57:49 +0100

    Updated tesseract (5.2.0 -> 5.3.0)

--- tests/pdf_extract_test.py
+++ tests/pdf_extract_test.py
@@ -9,5 +9,5 @@ def test_version():
 
 def test_pytesseract():
     assert set(pytesseract.get_languages()) >= {'eng', 'osd'}
-    assert pytesseract.get_tesseract_version().release >= (5, 2, 0)
-    assert pytesseract.get_tesseract_version().public == '5.2.0'
+    assert pytesseract.get_tesseract_version().release >= (5, 3, 0)
+    assert pytesseract.get_tesseract_version().public == '5.3.0'
