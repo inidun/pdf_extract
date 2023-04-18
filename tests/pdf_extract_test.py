e5f66cdde1f36167ace89ed8c1893cbf53206bec
aibakeneko <aibakeneko@gmail.com>
2023-04-18 13:30:13 +0200

    Renamed function test_pytesseract -> test_tesseract_installation

--- tests/pdf_extract_test.py
+++ tests/pdf_extract_test.py
@@ -7,7 +7,7 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
-def test_pytesseract():
+def test_tesseract_installation():
     assert set(pytesseract.get_languages()) >= {'eng', 'osd'}
     assert pytesseract.get_tesseract_version().release >= (5, 3, 0)
     assert pytesseract.get_tesseract_version().public == '5.3.1'
