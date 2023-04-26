ad4d49c79267f729b19ebdd448ead90a21f3f6c8
aibakeneko <aibakeneko@gmail.com>
2023-04-26 16:00:53 +0200

    Resolves #4

--- pdf_extract/tesseract_extractor.py
+++ pdf_extract/tesseract_extractor.py
@@ -1,8 +1,10 @@
 import os
 from dataclasses import dataclass
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Optional, Union
 
+import pdf2image
 import pytesseract
 from loguru import logger
 from pdf2image import convert_from_path
@@ -118,6 +120,64 @@ class TesseractExtractor(ITextExtractor):
                 fp.write(hocr)
 
         logger.success(f'Extracted: {basename}, pages: {i+1}, dpi: {self.dpi}, fmt: {self.fmt}')
+
+    def extract_text(
+        self,
+        filename: str | os.PathLike,
+        output_folder: str | os.PathLike,
+        first_page: int | None = 1,
+        last_page: int | None = None,
+        page_numbers: bool = False,
+    ) -> None:
+        first_page: int = first_page or 1
+        basename = Path(filename).stem
+
+        num_pages = pdf2image.pdfinfo_from_path(str(filename))['Pages']
+        if last_page is None or last_page > num_pages:
+            last_page = int(num_pages)
+
+        ouput_filename: str = (
+            f'{basename}_{first_page}-{last_page}.txt' if last_page < num_pages or first_page > 1 else f'{basename}.txt'
+        )
+
+        output_filepath: Path = Path(output_folder) / ouput_filename
+
+        if output_filepath.exists():
+            logger.info(f'Skipping {basename}: Already extracted')
+            return
+
+        logger.info(f'Processing {basename}, pages: {first_page}-{last_page}')
+
+        images = convert_from_path(
+            str(filename),
+            first_page=first_page,
+            last_page=last_page,  # type: ignore
+            dpi=self.dpi,
+            fmt=self.fmt,
+            grayscale=self.grayscale,
+            use_pdftocairo=self.use_pdftocairo,
+        )
+
+        with TemporaryDirectory() as temp_dir:
+            for i, image in enumerate(images):
+                text_filename = Path(temp_dir) / f'{basename}_{i+first_page:04}.txt'
+                with open(text_filename, 'w', encoding='utf-8') as fp:
+                    fp.write(pytesseract.image_to_string(image, lang='eng', config=self.tesseract_config))
+
+            with open(Path(output_folder) / ouput_filename, 'w', encoding='utf-8') as outfile:
+                if page_numbers:
+                    outfile.write(f'# {basename}\n\n')
+
+                for file in sorted(Path(temp_dir).glob('*.txt')):
+                    with open(file, 'r', encoding='utf-8') as infile:
+                        if page_numbers:
+                            page_number: int = int(file.stem.rsplit('_', 1)[-1])
+                            outfile.write(f'\n## Page {page_number}\n\n')
+
+                        contents = infile.read()
+                        outfile.write(contents)
+
+        logger.success(f'Extracted: {basename}, pages: {first_page}-{last_page}, dpi: {self.dpi}, fmt: {self.fmt}')
 
 
 if __name__ == '__main__':
--- tests/tesseract_extractor_test.py
+++ tests/tesseract_extractor_test.py
@@ -1,7 +1,10 @@
+import filecmp
 from difflib import SequenceMatcher
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import List
+
+import pytest
 
 from pdf_extract.interface import ITextExtractor
 from pdf_extract.tesseract_extractor import TesseractExtractor
@@ -46,3 +49,25 @@ def test_pdf_to_hocr_generates_expected_output():
 
         with open(result, 'r', encoding='utf-8') as r, open(expected, 'r', encoding='utf-8') as e:
             assert SequenceMatcher(None, r.read(), e.read()).quick_ratio() > 0.99
+
+
+@pytest.mark.skip('Files not guaranteed to be identical')
+@pytest.mark.parametrize(
+    'input_file, first_page, page_numbers, expected_filename',
+    [
+        ('pdf/2_pages_1_empty.pdf', 2, False, 'extract_text/2_pages_1_empty_2-2.txt'),
+        ('pdf/2_pages_1_empty.pdf', None, False, 'extract_text/2_pages_1_empty.txt'),
+        ('pdf/2_pages_1_empty.pdf', 2, True, 'extract_text_with_page_numbers/2_pages_1_empty_2-2.txt'),
+        ('pdf/2_pages_1_empty.pdf', None, True, 'extract_text_with_page_numbers/2_pages_1_empty.txt'),
+    ],
+)
+def test_extract_text_generates_expected_output(input_file, first_page, page_numbers, expected_filename):
+    file = test_dir / input_file
+    extractor: TesseractExtractor = TesseractExtractor(dpi=350, fmt='png')
+    expected = test_dir / 'expected/tesseract' / expected_filename
+
+    with TemporaryDirectory() as output_dir:
+        extractor.extract_text(file, output_dir, first_page=first_page, page_numbers=page_numbers)
+        result = Path(output_dir) / Path(expected_filename).name
+        assert result.exists()
+        assert filecmp.cmp(result, expected) is True
