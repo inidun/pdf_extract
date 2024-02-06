9a297d663f8c83fb5d6cc581a9716326da3606e5
aibakeneko <aibakeneko@gmail.com>
2024-02-06 16:16:29 +0100

    Updated Tesseract OCR (5.3.3 -> 5.3.4)

--- tests/tesseract_env_test.py
+++ tests/tesseract_env_test.py
@@ -8,4 +8,4 @@ def test_tesseract_language_support():
 
 def test_tesseract_version():
     assert pytesseract.get_tesseract_version().release >= (5, 3, 0)
-    assert pytesseract.get_tesseract_version().public == '5.3.3'
+    assert pytesseract.get_tesseract_version().public == '5.3.4'
