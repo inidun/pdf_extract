0db81059b338ee67cdf12c362b28eb4000f242c3
aibakeneko <aibakeneko@gmail.com>
2024-11-26 13:33:04 +0100

    Updated Tesseract version checks to require 5.5.0

--- tests/tesseract_env_test.py
+++ tests/tesseract_env_test.py
@@ -7,5 +7,5 @@ def test_tesseract_language_support():
 
 
 def test_tesseract_version():
-    assert pytesseract.get_tesseract_version().release >= (5, 4, 0)
-    assert pytesseract.get_tesseract_version().public == '5.4.1'
+    assert pytesseract.get_tesseract_version().release >= (5, 5, 0)
+    assert pytesseract.get_tesseract_version().public == '5.5.0'
